55435c6e785e4f9bcfbce164cc42fa5cec73452a
Sofaprinsen-to-og-nitti <freworr@gmail.com>
2014-02-11 18:25:13 +0100

    yuhu

--- slk/main.go
+++ slk/main.go
@@ -1,20 +1,21 @@
 package main
-//testing
+
 import(
     "os"
     "bufio"
+	"fmt"
 )
 
 func main(){
     out_socket, in_socket := connect()
-    go read(in_socket)
+    go recieve(in_socket)
     
     reader  := bufio.NewReader(os.Stdin)
     for{
         input,_ := reader.ReadString('\n')
         output  := string(input)
         for i:=0; i<5; i++ {
-            write(out_socket,output)
+            broadcast(out_socket,output)
         }
     }
     
--- slk/network.go
+++ slk/network.go
@@ -7,39 +7,37 @@ import(
 
 func connect()(*net.UDPConn, *net.UDPConn){
     localport      := "129.241.187.255:20016"
-    inAddr,_       := net.ResolveUDPAddr("udp4",localport)
-    
-    out_socket,err := net.DialUDP("udp4", nil, inAddr)
-    CheckError(err)
-    in_socket, err := net.ListenUDP("udp4", inAddr)
-    CheckError(err)
-    /*
-    out_socket,err := net.DialUDP("udp4",&net.UDPAddr{IP: net.IPv4(129,241,187,147)
-    
-    Port: 20016,},&net.UDPAddr{IP: net.IPv4(129,241,187,255), Port: 20016,})
-    CheckError(err)
-    in_socket, err  := net.ListenUDP("udp4",inAddr)
-    CheckError(err)
-    */
-/*yo*/
-    return out_socket, in_socket
+	for{
+    	inAddr,_       		:= net.ResolveUDPAddr("udp4",localport)
+	    out_socket,err_out 	:= net.DialUDP("udp4", nil, inAddr)
+	    in_socket, err_in 	:= net.ListenUDP("udp4", inAddr)
+
+	    if CheckError(err_out) && CheckError(err_in){
+			return out_socket, in_socket		
+		}
+	}    
 }
 
-func CheckError(err error){
+
+func CheckError(err error)(bool){
     if err!=nil{
         fmt.Println(err.Error())
-    }    
+		return false
+    }
+	return true
 }
 
-func write(out_socket *net.UDPConn, message string){
-    out_socket.Write([]byte(message+"\x00"))
+func broadcast(out_socket *net.UDPConn, message string){
+	out_socket.Write([]byte(message+"\x00"))
 }
 
-func read(in_socket *net.UDPConn){
+
+
+func recieve(in_socket *net.UDPConn){
     for{
         var buffer [1024]byte
-        n, _,_    := in_socket.ReadFromUDP(buffer[0:]) 
-        message      := string(buffer[0:n])
-        fmt.Println("read got:", message)
+        n, _,_		:= in_socket.ReadFromUDP(buffer[0:]) 
+        message		:= string(buffer[0:n])
+        fmt.Println("recieved:", message)
    }
 }
